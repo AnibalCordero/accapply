c2f0de175a517b90e64d019c36cf98faed0e0b30
Anibal <aniba.ac.cortez@gmail.com>
2022-12-15 01:04:36 -0300

    delete cors

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://d1x7s6ldgsxua1.cloudfront.net',
-    ],
-    methods: ["GET"],
-    credentials: true,
-  });
   await app.listen(3000);
 }
 bootstrap();
